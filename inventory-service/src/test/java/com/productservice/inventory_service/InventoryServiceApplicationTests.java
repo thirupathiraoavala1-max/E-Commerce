d02ff27f6610e2d928d8a6e107ed67c1c32f6ec1
Thirupathi Rao Avala <avala325@gmail.com>
2026-03-31 11:12:39 +0530

    These are services commits

---
+++ inventory-service/src/test/java/com/productservice/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productservice.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product-service/src/main/java/com/productservice/product_service/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.productservice.product_service.repository;
+
+import com.productservice.product_service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product,String> {
+
+}
