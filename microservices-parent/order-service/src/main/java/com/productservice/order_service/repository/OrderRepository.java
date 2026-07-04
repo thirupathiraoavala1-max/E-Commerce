7a75ed6d0a5b8f40c179d114c85abd1772b143f0
Thirupathi Rao Avala <thirupathiraoavala1@gmail.com>
2026-07-04 23:15:00 +0530

    Ecommerce Project Files

--- microservices-parent/order-service/src/main/java/com/productservice/order_service/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.productservice.order_service.repository;
-
-import com.productservice.order_service.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
